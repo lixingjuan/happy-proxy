179c780db6387b6a3738b4318c66a1becf5973ba
lixingjuan <xingjuan.li@datayes.com>
2022-01-16 07:45:13 +0800

    add: 项目初始化

--- src/background/index.js
+++ src/background/index.js
@@ -1,42 +1,21 @@
 import { apiRequest } from "@/api";
 
-/*global chrome*/
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        // 运行插件运行的页面URL规则
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({ pageUrl: {} }),
-        ],
-        actions: [new window.chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
-  });
-});
+const sourceUrl =
+  "https://gw.datayes-stg.com/ams_monitor_qa/web/industry/prosperity/comparison-chart/selection";
+const targetUrl =
+  "http://127.0.0.1:4000/ams_monitor_qa/web/industry/prosperity/comparison-chart/selection";
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // 接受来自content-script的消息，requset里不允许传递function类型的参数
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    const { contentRequest } = request;
-    // 接收来自content的api请求
-    if (contentRequest === "apiRequest") {
-      let { config } = request;
-      // API请求成功的回调
-      config.success = (data) => {
-        data.result = "succ";
-        sendResponse(data);
-      };
-      // API请求失败的回调
-      config.fail = (msg) => {
-        sendResponse({
-          result: "fail",
-          msg,
-        });
-      };
-      // 发起请求
-      apiRequest(config);
-    }
-  });
-  return true;
-});
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  (details) => {
+    console.log("onBeforeSendHeaders details", details);
+
+    return {
+      requestHeaders: details.requestHeaders,
+      redirectUrl: targetUrl,
+    };
+  },
+  {
+    urls: [sourceUrl],
+  },
+  ["blocking", "requestHeaders", "extraHeaders"]
+);
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,10 +1,27 @@
 import React, { useEffect } from "react";
 import "./popup.styl";
 
+const sourceUrl =
+  "https://gw.datayes-stg.com/ams_monitor_qa/web/industry/prosperity/comparison-chart/selection";
+const targetUrl =
+  "http://127.0.0.1:4000/ams_monitor_qa/web/industry/prosperity/comparison-chart/selection";
+
+const filter = `*://*/*`;
+
 function Popup() {
-  useEffect(() => {
-    // chrome.webRequest.onBeforeRequest(() => {}, "/demo", []);
-  }, []);
+  chrome.webRequest.onBeforeRequest.addListener(
+    (details) => {
+      console.log("onBeforeRequest", details);
+
+      return {
+        redirectUrl: sourceUrl,
+      };
+    },
+    {
+      urls: [sourceUrl],
+    },
+    ["blocking", "requestBody", "extraHeaders"]
+  );
 
   return (
     <div>
