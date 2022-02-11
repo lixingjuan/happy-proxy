82db4c6c0273a38682bda086bd96a8751b91c274
lixingjuan <xingjuan.li@datayes.com>
2022-02-11 15:26:50 +0800

    feat: 增加数据更新按钮

--- js/delete-button.js
+++ js/delete-button.js
@@ -1,50 +1,50 @@
-/*
+/**
  * @desc 删除全部button
- */
+ * */
 
-const buttonWrapper = document.querySelector(".button-wrapper");
+(function () {
+  /** button-list */
+  const buttonWrapper = document.getElementById("button-wrapper");
 
-/** delete all lacal cache data */
-function handleDeleteAll() {
-  fetch(apiQueryPatMap, {
-    method: "delete",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-  })
-    .then((res) => {
-      console.log(res);
-      const { status, statusText } = res;
-      if (status === 500) {
-        throw new Error(`${(status, statusText)}`);
-      }
-
-      const ul = document.querySelector(".table-list");
-      ul.innerHTML = "全部删除成功";
+  /** delete all lacal cache data */
+  function handleDeleteAll() {
+    fetch(apiQueryPathMap, {
+      method: "delete",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
     })
-    .catch((err) => {
-      pathMapContainer.innerHTML = `<h2 style='padding-left: 20px'> Error: ${err.message}</h2>`;
-    });
-}
+      .then((res) => {
+        console.log(res);
+        const { status, statusText } = res;
+        if (status === 500) {
+          throw new Error(`${(status, statusText)}`);
+        }
+        handleUpdate();
+        messages.success("删除成功");
+      })
+      .catch((err) => {
+        messages.error(`删除失败:${err.messages}`);
+      });
+  }
 
-/** append delete button */
-const button = document.createElement("button");
-const style = [
-  "position: fixed",
-  "right: 100px",
-  "z-index: 1000",
-  "opacity: 0.5",
-  "cursor: pointer",
-  "padding: 2px 8px",
-  "font-size: 14px",
-].join(";");
-button.innerText = "delete all";
-button.style = style;
-button.addEventListener("click", handleDeleteAll);
+  /** append delete button */
+  const button = document.createElement("button");
+  const style = [
+    "z-index: 1000",
+    "opacity: 0.5",
+    "cursor: pointer",
+    "padding: 2px 8px",
+    "font-size: 14px",
+  ].join(";");
+  button.innerText = "delete all";
+  button.style = style;
+  button.addEventListener("click", handleDeleteAll);
 
-buttonWrapper.appendChild(button);
+  buttonWrapper.appendChild(button);
+})();
---
+++ js/page-constant.js
@@ -0,0 +1,5 @@
+// 请求path-map json 接口
+const apiQueryPathMap = "http://127.0.0.1:4000/happy-service";
+
+// 内容区域
+const pathMapContainer = document.getElementById("path-map-container");
