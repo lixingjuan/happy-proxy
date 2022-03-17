b782984511944808e0cdd69cc1c86afda4f9b42e
lixingjuan <xingjuan.li@datayes.com>
2022-03-17 14:53:08 +0800

    feat: 🎉 增加本地服务状态提示

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,12 +1,13 @@
 import { Space } from "antd";
 import ConfigInput from "./ConfigInput";
 import SwitchButton from "./SwitchButton";
+import AlertError from "./AlertError";
 
 const Buttons = () => {
   return (
     <Space size="small">
+      <AlertError />
       <ConfigInput />
-
       <SwitchButton key="SwitchButton" />
     </Space>
   );
