9615fa544fe52d7f002b16adb0bdf7ae1ea35547
lixingjuan <18236129679@163.com>
2022-03-23 10:45:55 +0800

    fix: 修改打包输出目录为build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+  },
   resolve: {
     alias: {
       src: path.join(__dirname, "src"),
