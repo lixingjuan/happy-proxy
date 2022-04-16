acedd9dcb35f3b609683809b13ffab98403540e8
lixingjuan <18236129679@163.com>
2022-04-16 22:14:30 +0800

    feat: 迁移react18暂存

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./style/index.less";
 import "./assets/font/iconfont";
 import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
