15b90df2ce310e1395c2e97be705821e51ed6166
lixingjuan <18236129679@163.com>
2022-12-07 10:42:07 +0800

    fix: 尝试删除css和html相关worker 测试包体积

--- Chrome/popup.js
+++
@@ -1,5 +0,0 @@
-if (chrome?.tabs?.create) {
-  chrome.tabs.create({ url: chrome.extension.getURL('index.html') }, function (tab) {
-    // Tab opened.
-  });
-}
--- Proxy/src/components/Editor.tsx
+++ Proxy/src/components/Editor.tsx
@@ -4,20 +4,12 @@ import EditorReact, { loader } from '@monaco-editor/react';
 import { EditorProps, Monaco } from '@monaco-editor/react/lib/types.d';
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 window.MonacoEnvironment = {
   getWorker(_, label) {
     if (label === 'json') {
       return new jsonWorker();
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker();
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker();
     }
     if (label === 'typescript' || label === 'javascript') {
       return new tsWorker();
