9d108989d4ddb4f541786754c807c037cd3e22d9
lixingjuan <42627664+lixingjuan@users.noreply.github.com>
2023-01-17 01:58:07 +0000

    Deploying to main from @ lixingjuan/happy-proxy@6b5602b580fd2bd4457f82438f3d21e138eb3a32 🚀

---
+++ Chrome/assets/main.b0e48e87.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o,A as c}from"./App.fe11167c.js";const r=document.getElementById("root"),n=e.createRoot(r);n.render(t(o.StrictMode,{children:t(c,{})}));
---
+++ Chrome/assets/popup.ef929038.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,R as o,I as n}from"./App.fe11167c.js";const c=document.getElementById("root"),r=t.createRoot(c);r.render(e(o.StrictMode,{children:e("div",{className:"p-20",children:e(n,{showOpenTabButton:!0})})}));
